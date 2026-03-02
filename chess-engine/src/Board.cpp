8e51d5983457cadbcb9ee0786320c17a923d6c83
ivancc <crucescuetoivan@uma.es>
2026-03-02 11:47:33 +0100

    Adding CMakeLists.txt + Minor changes

--- chess-engine/src/Board.cpp
+++ chess-engine/src/Board.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Board.h"
+#include "Piece.h"
 #include <iostream>
 using namespace std;
 
@@ -18,30 +19,30 @@ void Board::initialize() {
     }
 
     //Rooks
-    squares[0][0] = squares[0][7] = Piece(PieceType::Rook, Color::White);
-    squares[7][0] = squares[7][7] = Piece(PieceType::Rook, Color::Black);
+    squares[0][0] = squares[0][7] = Piece(PieceType::Rook, PieceColor::White);
+    squares[7][0] = squares[7][7] = Piece(PieceType::Rook, PieceColor::Black);
 
     //Knights
-    squares[0][1] = squares[0][6] = Piece(PieceType::Knight, Color::White);
-    squares[7][1] = squares[7][6] = Piece(PieceType::Knight, Color::Black);
+    squares[0][1] = squares[0][6] = Piece(PieceType::Knight, PieceColor::White);
+    squares[7][1] = squares[7][6] = Piece(PieceType::Knight, PieceColor::Black);
 
     //Bishops
-    squares[0][2] = squares[0][5] = Piece(PieceType::Bishop, Color::White);
-    squares[7][2] = squares[7][5] = Piece(PieceType::Bishop, Color::Black);
+    squares[0][2] = squares[0][5] = Piece(PieceType::Bishop, PieceColor::White);
+    squares[7][2] = squares[7][5] = Piece(PieceType::Bishop, PieceColor::Black);
 
     //Queens
-    squares[0][3] = Piece(PieceType::Queen, Color::White);
-    squares[7][3] = Piece(PieceType::Queen, Color::Black);
+    squares[0][3] = Piece(PieceType::Queen, PieceColor::White);
+    squares[7][3] = Piece(PieceType::Queen, PieceColor::Black);
 
     //Kings
-    squares[0][4] = Piece(PieceType::King, Color::White);
-    squares[7][4] = Piece(PieceType::King, Color::Black);
+    squares[0][4] = Piece(PieceType::King, PieceColor::White);
+    squares[7][4] = Piece(PieceType::King, PieceColor::Black);
 }
 
 void Board::print() const {
     for (int row = 7; row >= 0; row--) {
         for (int col = 0; col < 8; col++) {
-            cout << static_cast<int>(squares[row][col].type); << " ";
+            cout << static_cast<int>(squares[row][col].type) << " ";
         }
         cout << endl;
     }
--- chess-engine/src/main.cpp
+++ chess-engine/src/main.cpp
@@ -2,3 +2,10 @@
 // Created by ivancc on 3/2/26.
 //
 
+#include "Board.cpp"
+
+int main() {
+    Board board;
+    board.print();
+    return 0;
+}
