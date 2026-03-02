3d6c21844a3debe723e939863ef5e9ead4c6dd3b
ivancc <crucescuetoivan@uma.es>
2026-03-02 11:21:49 +0100

    Modified “Piece.h” to have the structure for representing the pieces.

--- chess-engine/src/Piece.h
+++ chess-engine/src/Piece.h
@@ -4,5 +4,29 @@
 
 #ifndef ASTRACHESS_PIECE_H
 #define ASTRACHESS_PIECE_H
+#pragma once
+
+enum class PieceType {
+    None,
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King
+};
+
+enum class PieceColor {
+    White,
+    Black,
+    None
+};
+
+struct Piece {
+    PieceType type;
+    PieceColor color;
+    Piece(PieceType t = PieceType::None, PieceColor c = PieceColor::None):
+    type(t), color(c) {}
+};
 
 #endif //ASTRACHESS_PIECE_H
